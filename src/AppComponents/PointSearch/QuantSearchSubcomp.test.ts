d803f03d5df570d8352a39de662860d00c93c57d
Andrey G <andreyg@xello.world>
2021-08-06 14:18:50 -0400

    quantitative search tests PASS

--- src/AppComponents/PointSearch/QuantSearchSubcomp.test.ts
+++ src/AppComponents/PointSearch/QuantSearchSubcomp.test.ts
@@ -5,31 +5,173 @@ it('should exist if created', () => {
     expect(subcomp).not.toBeNull()
 })
 
-// ****** TEST CASES:  input -> list of options *******
+it('should return most/least if "!!" is supplied', () => {
+    let subcomp = new QuantSearchSubcomp();
+    let result = subcomp.getOptions('!!');
+    expect(result.queryValid).toBeTruthy();
+    expect(result.options.length).toBe(2);
+    expect(result.options[0].label).toBe('most');
+    expect(result.options[1].label).toBe('least');
+});
 
-// !! -> most/least
-// !!m -> most
-// !! mo -> most
-// !!    leas -> least
-// !! f -> INVALID QUERY
-// !! most -> positive/old/active/connected/recently responded
-// !! least -> positive/old/active/connected/recently responded
-// !! most p -> positive
-// !! least   pos -> positive
-// !! least v -> INVALID QUERY
+it('should return most if "!!m" is supplied', () => {
+    let subcomp = new QuantSearchSubcomp();
+    let result = subcomp.getOptions('!!m');
+    expect(result.queryValid).toBeTruthy();
+    expect(result.options.length).toBe(1);
+    expect(result.options[0].label).toBe('most');
+});
 
-// !! most old; -> most/least
-// !! most old; least   pos -> positive
-// !! most old; mo -> most
-// !! most old; least -> positive/active/connected/recently responded
-// !! most old; least v -> INVALID QUERY
+it('should return most if "!! mo" is supplied', () => {
+    let subcomp = new QuantSearchSubcomp();
+    let result = subcomp.getOptions('!!mo');
+    expect(result.queryValid).toBeTruthy();
+    expect(result.options.length).toBe(1);
+    expect(result.options[0].label).toBe('most');
+});
 
-// !! most old; least connected; least -> positive/active/recently responded
+it('should return least if "!!    leas" is supplied', () => {
+    let subcomp = new QuantSearchSubcomp();
+    let result = subcomp.getOptions('!!    leas');
+    expect(result.queryValid).toBeTruthy();
+    expect(result.options.length).toBe(1);
+    expect(result.options[0].label).toBe('least');
+});
 
-// !!most connected; least connected -> INVALID QUERY
-// !!most connected; least c -> INVALID QUERY
-// !!most connected; least con -> INVALID QUERY
-// !! most old; least connected; least connected -> INVALID QUERY
+it('should return INVALID QUERY if "!!  z" is supplied', () => {
+    let subcomp = new QuantSearchSubcomp();
+    let result = subcomp.getOptions('!! z');
+    expect(result.queryValid).toBeFalsy();
+    expect(result.options.length).toBe(0);
+});
+
+it('should return positive/old/active/connected/fresh if "!! most" is supplied', () => {
+    let subcomp = new QuantSearchSubcomp();
+    let result = subcomp.getOptions('!! most');
+    expect(result.queryValid).toBeTruthy();
+    expect(result.options.length).toBe(5);
+    expect(result.options[0].label).toBe('active');
+    expect(result.options[1].label).toBe('connected');
+    expect(result.options[2].label).toBe('old');
+    expect(result.options[3].label).toBe('positive');
+    expect(result.options[4].label).toBe('fresh');
+});
+
+it('should return positive/old/active/connected/fresh if "!! least" is supplied', () => {
+    let subcomp = new QuantSearchSubcomp();
+    let result = subcomp.getOptions('!! least');
+    expect(result.queryValid).toBeTruthy();
+    expect(result.options.length).toBe(5);
+    expect(result.options[0].label).toBe('active');
+    expect(result.options[1].label).toBe('connected');
+    expect(result.options[2].label).toBe('old');
+    expect(result.options[3].label).toBe('positive');
+    expect(result.options[4].label).toBe('fresh');
+});
+
+it('should return positive if "!! most p" is supplied', () => {
+    let subcomp = new QuantSearchSubcomp();
+    let result = subcomp.getOptions('!! most p');
+    expect(result.queryValid).toBeTruthy();
+    expect(result.options.length).toBe(1);
+    expect(result.options[0].label).toBe('positive');
+});
+
+it('should return positive if "!! least pos" is supplied', () => {
+    let subcomp = new QuantSearchSubcomp();
+    let result = subcomp.getOptions('!! least pos');
+    expect(result.queryValid).toBeTruthy();
+    expect(result.options.length).toBe(1);
+    expect(result.options[0].label).toBe('positive');
+});
+
+it('should return INVALID QUERY if "!! least v" is supplied', () => {
+    let subcomp = new QuantSearchSubcomp();
+    let result = subcomp.getOptions('! least v');
+    expect(result.queryValid).toBeFalsy();
+    expect(result.options.length).toBe(0);
+});
+
+it('should return most/least if "!! most old;" is supplied', () => {
+    let subcomp = new QuantSearchSubcomp();
+    let result = subcomp.getOptions('!! most old;');
+    expect(result.queryValid).toBeTruthy();
+    expect(result.options.length).toBe(2);
+    expect(result.options[0].label).toBe('most');
+    expect(result.options[1].label).toBe('least');
+});
+
+it('should return positive if "!! most old; least pos" is supplied', () => {
+    let subcomp = new QuantSearchSubcomp();
+    let result = subcomp.getOptions('!! most old; least pos');
+    expect(result.queryValid).toBeTruthy();
+    expect(result.options.length).toBe(1);
+    expect(result.options[0].label).toBe('positive');
+});
+
+it('should return positive if "!! most old;   mo" is supplied', () => {
+    let subcomp = new QuantSearchSubcomp();
+    let result = subcomp.getOptions('!! most old;   mo');
+    expect(result.queryValid).toBeTruthy();
+    expect(result.options.length).toBe(1);
+    expect(result.options[0].label).toBe('most');
+});
+
+it('should return positive/active/connected/fresh if "!! most old; least" is supplied', () => {
+    let subcomp = new QuantSearchSubcomp();
+    let result = subcomp.getOptions('!! most old; least');
+    expect(result.queryValid).toBeTruthy();
+    expect(result.options.length).toBe(4);
+    expect(result.options[0].label).toBe('active');
+    expect(result.options[1].label).toBe('connected');
+    expect(result.options[2].label).toBe('positive');
+    expect(result.options[3].label).toBe('fresh');
+});
+
+it('should return INVALID QUERY if "!! most old; least v" is supplied', () => {
+    let subcomp = new QuantSearchSubcomp();
+    let result = subcomp.getOptions('!! most old; least v');
+    expect(result.queryValid).toBeFalsy();
+    expect(result.options.length).toBe(0);
+});
+
+it('should return positive/active/fresh responded if "!! most old; least connected; least" is supplied', () => {
+    let subcomp = new QuantSearchSubcomp();
+    let result = subcomp.getOptions('!! most old; least connected; least');
+    expect(result.queryValid).toBeTruthy();
+    expect(result.options.length).toBe(3);
+    expect(result.options[0].label).toBe('active');
+    expect(result.options[1].label).toBe('positive');
+    expect(result.options[2].label).toBe('fresh');
+});
+
+it('should return INVALID QUERY if "!!most connected; least connected" is supplied', () => {
+    let subcomp = new QuantSearchSubcomp();
+    let result = subcomp.getOptions('!!most connected; least connected');
+    expect(result.queryValid).toBeFalsy();
+    expect(result.options.length).toBe(0);
+});
+
+it('should return INVALID QUERY if "!!most connected; least c" is supplied', () => {
+    let subcomp = new QuantSearchSubcomp();
+    let result = subcomp.getOptions('!!most connected; least c');
+    expect(result.queryValid).toBeFalsy();
+    expect(result.options.length).toBe(0);
+});
+
+it('should return INVALID QUERY if "!!most connected; least con" is supplied', () => {
+    let subcomp = new QuantSearchSubcomp();
+    let result = subcomp.getOptions('!!most connected; least con');
+    expect(result.queryValid).toBeFalsy();
+    expect(result.options.length).toBe(0);
+});
+
+it('should return INVALID QUERY if "!! most old; least connected" is supplied', () => {
+    let subcomp = new QuantSearchSubcomp();
+    let result = subcomp.getOptions('!! most old; least connected');
+    expect(result.queryValid).toBeTruthy();
+    expect(result.options.length).toBe(0);
+});
 
 export {}
 
@@ -41,7 +183,7 @@ export {}
 *   - date of creation (old)
 *   - activity (active)
 *   - connectivity (connected)
-*   - date of the last response (recently responded)
+*   - date of the last response (fresh)
 * More criteria can be added so the design should allow somewhat easy additions. To facilitate this, a framework is
 * required. It can look something like this:
 *
--- src/AppComponents/PointSearch/QuantSearchSubcomp.ts
+++ src/AppComponents/PointSearch/QuantSearchSubcomp.ts
@@ -2,14 +2,135 @@ import {FoundPoint} from "../../AppState/FoundPoint";
 
 export class QuantOptionsResult {
     public options: FoundPoint[] = [];
-    public queryValid: boolean = false;
+    public queryValid: boolean = true;
 }
 
 export class QuantSearchSubcomp {
     public signalToken: string = '!!';
+    private qualifiers: string[] = ['most', 'least'];
+    private specifiers: string[] = ['active', 'connected', 'old', 'positive', 'fresh'];
+    private usedSpecifiers: string[] = [];
 
     getOptions(query: string): QuantOptionsResult {
         let result = new QuantOptionsResult();
+
+        query = query.replace('!!', '');
+        let words: string[] = [];
+        let word: string = '';
+        for(let char of query) {
+            if (char !== ' ' && char !== ';') {
+                word+= char;
+            } else if (char === ';') {
+                if (word !== '') {
+                    words.push(word.slice());
+                    word = '';
+                }
+                words.push(';')
+            } else {
+                if (word !== '') {
+                    words.push(word.slice());
+                    word = '';
+                }
+            }
+        }
+        if (word !== '') {
+            words.push(word.slice());
+            word = '';
+        }
+
+        words = words.filter(w => w !== '');
+        // trivial: nothing in the query
+        if (words.length < 1) {
+            result.options = this.qualifiers.map(q => {
+                let p = new FoundPoint();
+                p.label = q;
+                return p;
+            });
+            return result;
+        }
+
+        let currentQualifier = '';
+        let currentSpecifier = '';
+        words.forEach((word, idx) => {
+            if (idx !== words.length - 1) {
+                // check validity
+                if (this.qualifiers.some(q => q === word)) {
+                    currentQualifier = word;
+                } else if (this.specifiers.some(q => q === word) && currentQualifier !== '') {
+                    currentSpecifier = word;
+                } else if (word === ';') {
+                    //do nothing
+                } else {
+                    result.queryValid = false;
+                }
+
+                if (currentQualifier !== '' && currentSpecifier !== '') {
+                    this.usedSpecifiers.push(currentSpecifier.slice());
+                    currentQualifier = '';
+                    currentSpecifier = '';
+                }
+            } else {
+                // last word; populate suggestions
+                for(let q of this.qualifiers) {
+                    if (q === word) {
+                        for(let s of this.specifiers) {
+                            if (!this.usedSpecifiers.some(used => used === s)) {
+                                let point = new FoundPoint();
+                                point.label = s;
+                                result.options.push(point);
+                            }
+                        }
+                        result.queryValid = true;
+                        break;
+                    } else if (q.startsWith(word) && q !== word) {
+                        let point = new FoundPoint();
+                        point.label = q;
+                        result.options.push(point);
+                        result.queryValid = true;
+                        break;
+                    } else {
+                        result.queryValid = false;
+                    }
+                }
+
+                if (result.options.length > 0) {
+                    return result;
+                }
+
+                for(let s of this.specifiers) {
+                    if (s === word) {
+                        this.usedSpecifiers.push(s);
+                        result.queryValid = true;
+                        break;
+                    } else if (s.startsWith(word) && s !== word && !this.usedSpecifiers.some(used => used === s)) {
+                        let point = new FoundPoint();
+                        point.label = s;
+                        result.options.push(point);
+                        result.queryValid = true;
+                        break;
+                    } else {
+                        result.queryValid = false;
+                    }
+                }
+
+                if (word === ';') {
+                    for (let q of this.qualifiers) {
+                        let point = new FoundPoint();
+                        point.label = q;
+                        result.options.push(point);
+                        result.queryValid = true;
+                    }
+                }
+            }
+        });
+
+        // check duplicates
+        let findDuplicates = arr => arr.filter((item, index) => arr.indexOf(item) !== index)
+        let dups = findDuplicates(this.usedSpecifiers);
+        if (dups.length > 0) {
+            result.queryValid = false;
+            result.options = [];
+        }
 
         return result;
     }
