c5cf60f8a80354961fbb5dc728a16563e80a9309
Andrey G <andreyg@xello.world>
2021-09-20 13:33:25 -0400

    status light wiring for hive operations

--- src/AppComponents/FeedbackBar/FeedbackBarComponent.tsx
+++ src/AppComponents/FeedbackBar/FeedbackBarComponent.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import {Keyframes} from "react-spring/renderprops";
+import {Subscription} from "rxjs";
+import {ActiveHiveState} from "../../AppState/ActiveHiveState";
+import {LoadingStatus} from "../../AppState/LoadingStatus";
+import {StatefulObject} from "../../AppState/StatefulObject";
 
 const Blinker: any = Keyframes.Spring({
     blinkSuccess: [
@@ -26,12 +30,54 @@ const Blinker: any = Keyframes.Spring({
 });
 
 class FeedbackBarComponent extends React.Component<any, any> {
-    render() {
-        let state = this.props.status;
-        let action = 'blinkSuccess';
+    private subs: Subscription[] = [];
 
+    constructor(props: any) {
+        super(props);
+
+        this.state = {
+            action: 'blinkSuccess'
+        }
+    }
+
+    componentDidMount() {
+        this.subs.push(
+            ActiveHiveState.subgraph.statusUpdatedEvent.subscribe((status) => {
+                this.activateBlinker(status, ActiveHiveState.subgraph);
+            })
+        );
+        this.subs.push(
+            ActiveHiveState.activeHiveManifest.statusUpdatedEvent.subscribe((status) => {
+                this.activateBlinker(status, ActiveHiveState.activeHiveManifest);
+            })
+        );
+        this.subs.push(
+            ActiveHiveState.lastAddedItem.statusUpdatedEvent.subscribe((status) => {
+                this.activateBlinker(status, ActiveHiveState.lastAddedItem);
+            })
+        );
+        this.subs.push(
+            ActiveHiveState.foundPoints.statusUpdatedEvent.subscribe((status) => {
+                this.activateBlinker(status, ActiveHiveState.foundPoints);
+            })
+        );
+        this.subs.push(
+            ActiveHiveState.newPoint.statusUpdatedEvent.subscribe((status) => {
+                this.activateBlinker(status, ActiveHiveState.newPoint);
+            })
+        );
+    }
+
+    componentWillUnmount() {
+        for(let sub of this.subs) {
+            sub.unsubscribe();
+        }
+        this.subs = [];
+    }
+
+    render() {
         return (
-            <Blinker state={action} config={{duration: 150}}>
+            <Blinker state={this.state.action} config={{duration: 150}}>
                 {(props: any) => (
                     <div style={{
                         width: '100%',
@@ -42,6 +88,39 @@ class FeedbackBarComponent extends React.Component<any, any> {
                 )}
             </Blinker>
         )
+    }
+
+    private activateBlinker(status: LoadingStatus, target: StatefulObject<any>) {
+        if (status === LoadingStatus.Pending) {
+            this.setStateToPending();
+        } else if (status === LoadingStatus.Error) {
+            this.setStateToFail();
+            this.displayError(target.error);
+        } else {
+            this.setStateToSuccess();
+        }
+    }
+
+    private setStateToSuccess() {
+        this.setState({
+            action: 'blinkSuccess'
+        });
+    }
+
+    private setStateToPending() {
+        this.setState({
+            action: 'blinkLoading'
+        });
+    }
+
+    private setStateToFail() {
+        this.setState({
+            action: 'blinkError'
+        });
+    }
+
+    private displayError(error: string) {
+        console.log(error);
     }
 }
 
